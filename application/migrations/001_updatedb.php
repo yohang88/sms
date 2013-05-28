2e983df778a26fdb2595638cc2a19b9c8a14030f
Yoga Hanggara <yogahanggara@gmail.com>
2013-05-28 09:33:40 +0700

    add migration script for template feature

--- application/migrations/001_updatedb.php
+++ application/migrations/001_updatedb.php
@@ -19,15 +19,15 @@ class Migration_UpdateDB extends CI_Migration {
         $this->dbforge->create_table('config');
 
         $data = array(
-         array(
-          'key' => 'sms_signature',
-          'params' => 'mySMS Center'
-          ),
-         array(
-          'key' => 'sms_signature_enable',
-          'params' => '0'
-          )
-         );
+           array(
+              'key' => 'sms_signature',
+              'params' => 'mySMS Center'
+              ),
+           array(
+              'key' => 'sms_signature_enable',
+              'params' => '0'
+              )
+           );
 
         $this->db->insert_batch('config', $data);
 
@@ -41,11 +41,40 @@ class Migration_UpdateDB extends CI_Migration {
             );
         $this->dbforge->add_column('log', $fields);
 
+
+        // Template Table
+        $this->dbforge->add_field('id');
+        $this->dbforge->add_field(array(
+            'title' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '200'
+                ),
+            'content' => array(
+                'type' => 'TEXT',
+                'null' => TRUE
+                )
+            ));
+
+        $this->dbforge->create_table('templates');
+
+        $data = array(
+           array(
+              'title' => "Terimakasih Saran dan Kritik",
+              'content' => "Terimakasih banyak atas saran dan kritik yang Anda berikan. Kami akan segera menindaklanjuti hal tersebut."
+              ),
+           array(
+              'title' => "Pemberitahuan",
+              'content' => "Dengan ini kami memberitahukan bahwa kami akan menyelenggarakan kegiatan [Nama Kegiatan] pada [Hari], [Tanggal] dan bertempat di [Lokasi]. Kami mengharapkan partisipasi dari Anda.\n\nTerimakasih banyak."
+              )
+           );
+
+        $this->db->insert_batch('templates', $data);
     }
 
     public function down()
     {
         $this->dbforge->drop_table('config');
+        $this->dbforge->drop_table('templates');
         $this->dbforge->drop_column('log', 'flag');
     }
 
