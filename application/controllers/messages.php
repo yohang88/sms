268df31b1b5821792ce8e883e8a8680969bb4d13
Yoga Hanggara <yogahanggara@gmail.com>
2013-05-31 11:11:21 +0700

    remove open flash chart

--- application/controllers/messages.php
+++ application/controllers/messages.php
@@ -186,38 +186,6 @@ class Messages extends CI_Controller {
 
     public function statistic()
     {
-        $this->load->library('OpenFlashChartLib', NULL, 'OFCL');
-        $points = 10;
-        $x = array(6);
-        $yout = array(1);
-        $yin = array(1);
-        $data_1 = array(1,2,3,4);
-        $data_2 = array(1,2,3,4);
-        $data_3 = array(1,2,3,4);
 
-
-        $bar_1 = new bar();
-        $bar_1->set_values(50);
-        $bar_1->set_colour('#639F45');
-        $bar_1->key('incoming', 10 );
-
-        $bar_2 = new bar();
-        $bar_2->set_values(20);
-        $bar_2->set_colour('#21759B');
-        $bar_2->key('outgoing', 10 );
-
-        $y = new y_axis();
-        $y->set_range(0, 100, 10);
-
-        $element1 = $bar_1;
-        $element2 = $bar_2;
-
-        $chart = new open_flash_chart();
-        $chart->add_element($element1);
-        $chart->add_element($element2);
-        $chart->set_y_axis($y);
-
-
-        echo $chart->toPrettyString();
     }
 }
